a1c4db73c4151e0bf51a84105becfc366911d636
Elliot Ng <chihimng.io@gmail.com>
2020-02-06 22:48:03 +0800

    feat: implement ui for login

--- mobile/lib/pages/infomation_page.dart
+++ mobile/lib/pages/infomation_page.dart
@@ -12,8 +12,76 @@ class InformationPage extends StatefulWidget {
 class _InformationPageState extends State<InformationPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Wow, so much information, much wow"),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: EdgeInsets.all(24.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,              
+                children: <Widget>[
+                  Text(
+                    "Getting Started",
+                    style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.w500)
+                  ),
+                  Text(
+                    "Login to access more features.",
+                    style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w300),
+                  )
+                ],
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 12.0)),
+            Container(
+              width: double.infinity,
+              child: TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: InputDecoration(labelText: "Phone Number"),
+              ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Expanded(
+                  flex: 7,
+                  child: TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Verification Code"),
+                  )
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Container(
+                    child: RaisedButton(
+                      child: Text("Get Code"),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+                      onPressed: () {},
+                    )
+                  )
+                )
+              ],
+            ),
+            Padding(padding: EdgeInsets.only(top: 12.0)),
+            Container(
+              width: double.infinity,
+              height: 40.0,
+              child: RaisedButton(
+                child: Text("Login"),
+                color: Colors.blueAccent,
+                textColor: Colors.white,
+                onPressed: () {},
+              ),
+            )
+          ],
+        )
+      )
     );
   }
 }

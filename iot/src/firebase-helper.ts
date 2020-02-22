dd3a474b63263984a346a0fa0658af934c578a27
Esmond <esmond9981127@gmail.com>
2020-02-23 00:38:23 +0800

    wip: migration

--- iot/src/firebase-helper.ts
+++ iot/src/firebase-helper.ts
@@ -8,6 +8,7 @@ import { IotState } from "./iot-state";
 export class FirebaseHelper {
     deviceId: string;
     firestore: firebase.firestore.Firestore;
+    functions: firebase.functions.Functions;
 
     constructor(deviceId) {
         this.deviceId = deviceId;
@@ -17,10 +18,18 @@ export class FirebaseHelper {
         try {
             //FIXME: Security
             let app = firebase.initializeApp({
-                project_id: "fyp-smartcarpark",
+                apiKey: "AIzaSyCJOspIynTav487E4qnKkj-o8WHTsddGIQ",
+                authDomain: "fyp-smartcarpark.firebaseapp.com",
+                databaseURL: "https://fyp-smartcarpark.firebaseio.com",
+                projectId: "fyp-smartcarpark",
+                storageBucket: "fyp-smartcarpark.appspot.com",
+                messagingSenderId: "777981643981",
+                appId: "1:777981643981:web:6f215aa76849c60389ad84",
+                measurementId: "G-TRQVENNKGS"
             })
             await firebase.auth().signInWithEmailAndPassword("iot-client@fyp-smartcarpark.com", "f]T*UjCtLGf7[#TYxJQ;vJ");
             this.firestore = app.firestore();
+            this.functions = app.functions('asia-east-2');
         } catch (error) {
             console.log(error);
         }
@@ -36,9 +45,11 @@ export class FirebaseHelper {
                 imageTimestamp: iotState.time.toISOString()
             }
 
-            let uploadFunction = firebase.functions().httpsCallable('iotUploadSnapshot');
+            let uploadFunction = this.functions.httpsCallable('iotUploadSnapshot');
             try {
-                imageUrl = (await uploadFunction(JSON.stringify(data))).data["imageUrl"];
+                let response = await uploadFunction(data);
+                console.log(response);
+                imageUrl = response.data["imageUrl"];
             } catch (error) {
                 console.log(error);
             }

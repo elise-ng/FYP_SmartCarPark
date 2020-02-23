8dd73b29a1be0dc134f632b83d0ab94a1bc285c7
Esmond <esmond9981127@gmail.com>
2020-02-24 02:15:18 +0800

    feat: use listener and interval combo for echo measurement

--- iot/src/index.ts
+++ iot/src/index.ts
@@ -1,42 +1,18 @@
 import { StillCamera } from "pi-camera-connect";
-import moment from "moment";
 import { FirebaseHelper } from "./firebase-helper";
 import { IotState, ParkingStatus } from "./iot-state";
+import { Gpio } from "pigpio";
 
-const Gpio = require('pigpio').Gpio;
-
+// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
+const MICROSECDONDS_PER_CM = 1e6 / 34321;
 // Setting up GPIO
+let trigger = new Gpio(4, { mode: Gpio.OUTPUT });
+let echo = new Gpio(17, { mode: Gpio.INPUT, alert: true });
 
 // Setting up Camera
 let camera = new StillCamera();
 
-function sleep(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-function getDistance() {
-  return new Promise((resolve, reject) => {
-    var MICROSECDONDS_PER_CM = 1e6 / 34321;
-    var trigger = new Gpio(4, { mode: Gpio.OUTPUT });
-    var echo = new Gpio(17, { mode: Gpio.INPUT, alert: true });
-    trigger.digitalWrite(0); // Make sure trigger is low
-    var startTick;
-    var prox;
-    trigger.trigger(10, 1);
-    echo.on('alert', (level, tick) => {
-      if (level == 1) {
-        startTick = tick;
-      } else {
-        var endTick = tick;
-        var diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit $
-        prox = diff / 2 / MICROSECDONDS_PER_CM;
-        resolve(prox);
-      }
-    });
-  });
-}
-
-
+// Capture image
 async function capture(): Promise<Buffer> {
     return camera.takeImage();
 }
@@ -45,22 +21,34 @@ async function main() {
     let parked = false;
     let firebaseHelper = new FirebaseHelper('lg5-1');
     await firebaseHelper.init();
-    console.log("done firebase helper")
-    while(true) {
-    console.log("enter loop")
-        const dist = await getDistance();
+
+    // Echo distance calculation listener
+    let startTick;
+    trigger.digitalWrite(0); // Make sure trigger is low
+    echo.on('alert', async (level, tick) => {
+      if (level == 1) {
+        startTick = tick;
+      } else {
+        let endTick = tick;
+        let diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+        let dist = diff / 2 / MICROSECDONDS_PER_CM;
 
         console.log(`Measured Distance: ${dist} cm`);
         if(dist < 30 && !parked) {
+            parked = true;
             let imageBuffer = await capture();
             let iotState = new IotState("test_vehicle_id", ParkingStatus.Occupied);
             firebaseHelper.updateIotState(iotState, imageBuffer);
         } else if (dist > 30 && parked) {
             parked = false;
-        } else {
-            await sleep(1000);
         }
-    }
+      }
+    });
+
+    // Trigger a distance measurement once per second
+    setInterval(() => {
+      trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+    }, 1000);
 }
 
 main();

7ae726d06d5d40c86e0954da71c3614e37648b28
Esmond <esmond9981127@gmail.com>
2020-04-10 18:01:30 +0800

    fix: credit card page UI

--- mobile/lib/pages/payment/credit_card.dart
+++ mobile/lib/pages/payment/credit_card.dart
@@ -1,58 +1,61 @@
+import 'package:awesome_card/credit_card.dart';
 import 'package:flutter/material.dart';
 import 'package:awesome_card/awesome_card.dart' as card;
 import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class CardPage extends StatefulWidget {
   String gateRecordId;
-  int fee; 
+  int fee;
+
   CardPage({
     key,
     gateRecordId,
     fee,
-  }) : this.gateRecordId = gateRecordId, this.fee = fee, super(key: key);
+  })  : this.gateRecordId = gateRecordId,
+        this.fee = fee,
+        super(key: key);
 
   @override
   _CardPageState createState() => _CardPageState();
 }
 
 class _CardPageState extends State<CardPage> {
-    String carPlateNum = "PK12345";
-    String parkingLocation = "LG3-12";
-    int parkingDuation = 130;
-    String cardNumber = '';
-    String expiryDate = '';
-    String cardHolderName = '';
-    String cvvCode = '';
-    bool isCvvFocused = false;
+  String carPlateNum = "PK12345";
+  String parkingLocation = "LG3-12";
+  int parkingDuation = 130;
+  String cardNumber = '';
+  String expiryDate = '';
+  String cardHolderName = '';
+  String cvvCode = '';
+  bool isCvvFocused = false;
 
-    // String cardNumber = "";
-    // String cardHolderName = "";
-    // String expiryDate = "";
-    String cvv = "";
-    bool showBack = false;
+  // String cardNumber = "";
+  // String cardHolderName = "";
+  // String expiryDate = "";
+  String cvv = "";
+  bool showBack = false;
 
-    FocusNode _focusNode;
+  FocusNode _focusNode;
 
-    var controller = new MaskedTextController(mask: '00/00');
+  MaskedTextController _expiryTextController =
+      MaskedTextController(mask: '00/00');
 
-
-    @override
-    void initState() {
-      super.initState();
-      _focusNode = new FocusNode();
-      _focusNode.addListener(() {
-        setState(() {
-          _focusNode.hasFocus ? showBack = true : showBack = false;
-        });
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = new FocusNode();
+    _focusNode.addListener(() {
+      setState(() {
+        _focusNode.hasFocus ? showBack = true : showBack = false;
       });
-    }
+    });
+  }
 
-    @override
-    void dispose() {
-      _focusNode.dispose();
-      super.dispose();
-    }
-
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,111 +65,111 @@ class _CardPageState extends State<CardPage> {
           title: new Text('Credit Card'),
           leading: new IconButton(
             icon: new Icon(Icons.arrow_back),
-                  onPressed: () {
-                    Navigator.pop(context);
-              },
+            onPressed: () {
+              Navigator.pop(context);
+            },
           ),
         ),
-        body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            SizedBox(
-              height: 20,
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: CreditCard(
+                cardNumber: cardNumber,
+                cardExpiry: expiryDate,
+                cardHolderName: cardHolderName,
+                cvv: cvv,
+                bankName: "",
+                showBackSide: showBack,
+                frontBackground: card.CardBackgrounds.black,
+                backBackground: card.CardBackgrounds.white,
+                showShadow: true,
+              ),
             ),
-            card.CreditCard(
-              cardNumber: cardNumber,
-              cardExpiry: expiryDate,
-              cardHolderName: cardHolderName,
-              cvv: cvv,
-              bankName: "",
-              showBackSide: showBack,
-              frontBackground: card.CardBackgrounds.black,
-              backBackground: card.CardBackgrounds.white,
-              showShadow: true,
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(hintText: "Card Number"),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(labelText: "Card Number"),
                     maxLength: 19,
+                    keyboardType: TextInputType.number,
                     onChanged: (value) {
                       setState(() {
                         cardNumber = value;
                       });
                     },
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 20,
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 14.0),
+                    child: TextFormField(
+                      decoration:
+                          InputDecoration(labelText: "Card Holder Name"),
+                      onChanged: (value) {
+                        setState(() {
+                          cardHolderName = value;
+                        });
+                      },
+                    ),
                   ),
-                  child: TextFormField(
-                    controller: controller,
-                    decoration: InputDecoration(hintText: "Card Expiry"),
-                    maxLength: 5,
-                    onChanged: (value) {
-                      setState(() {
-                        expiryDate = value;
-                      });
-                    },
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(
-                    horizontal: 20,
-                  ),
-                  child: TextFormField(
-                    decoration: InputDecoration(hintText: "Card Holder Name"),
-                    onChanged: (value) {
-                      setState(() {
-                        cardHolderName = value;
-                      });
-                    },
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-                  child: TextFormField(
-                    decoration: InputDecoration(hintText: "CVV"),
-                    maxLength: 3,
-                    onChanged: (value) {
-                      setState(() {
-                        cvv = value;
-                      });
-                    },
-                    focusNode: _focusNode,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.symmetric(horizontal: 40),
-                  child: RaisedButton( 
-                        color: Theme.of(context).primaryColor, 
-                        textColor: Colors.white, 
-                        child: Row(mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Pay Now with Credit Card"),
-                        Icon(Icons.navigate_next)                          
-                      ], ), 
-                        onPressed: () => {}, 
-                        splashColor: Colors.redAccent,
+                  Row(
+                    children: <Widget>[
+                      Flexible(
+                        flex: 2,
+                        child: Padding(
+                          padding: const EdgeInsets.only(right: 4.0),
+                          child: TextFormField(
+                            controller: this._expiryTextController,
+                            decoration: InputDecoration(labelText: "Expiry Date"),
+                            maxLength: 5,
+                            onChanged: (value) {
+                              setState(() {
+                                expiryDate = value;
+                              });
+                            },
+                          ),
+                        ),
                       ),
+                      Flexible(
+                        flex: 1,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 4.0),
+                          child: TextFormField(
+                            decoration: InputDecoration(labelText: "CVV"),
+                            maxLength: 3,
+                            onChanged: (value) {
+                              setState(() {
+                                cvv = value;
+                              });
+                            },
+                            focusNode: _focusNode,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              child: RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Pay Now with Credit Card"),
+                    Icon(Icons.navigate_next)
+                  ],
                 ),
-              ],
+                onPressed: () => {},
+                splashColor: Colors.redAccent,
+              ),
             )
           ],
         ),
-      ),
       ),
     );
   }

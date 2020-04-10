648e8d6c9a9b6742c082f8de46638a805cb82d81
cheungkayigrace <gracecheung1203@gmail.com>
2020-04-11 03:46:25 +0800

    delete comments

--- mobile/lib/pages/timeline_page.dart
+++ mobile/lib/pages/timeline_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 import 'package:smart_car_park_app/widgets/timeline_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -15,30 +14,7 @@ class TimeLinePage extends StatefulWidget {
   _TimeLinePageState createState() => _TimeLinePageState();
 }
 
-class IotStates {
-  final CollectionReference iotStateCollection = Firestore.instance.collection("iotStates");
-
-  Stream<QuerySnapshot> get iotStates{
-    return iotStateCollection.snapshots();
-  }
-}
-
-class UserRecords {
-  final CollectionReference userRecordsCollection = Firestore.instance.collection("userRecords");
-
-  Stream<QuerySnapshot> get iotStates{
-    return userRecordsCollection.snapshots();
-  }
-}
-
-
 class _TimeLinePageState extends State<TimeLinePage> {
-
-//  Future<List<DocumentSnapshot>> getVehicleID() async{
-//    var data = await Firestore.instance.collection('userRecords').getDocuments();
-//    var vehicleID = data.documents;
-//    return vehicleID;
-//  }
 
   @override
   void initState() {
@@ -46,141 +22,51 @@ class _TimeLinePageState extends State<TimeLinePage> {
     this._getData();
   }
 
-//  void getData() async {
-////    print("test");
-////    Firestore.instance
-////        .collection('iotStateChanges')
-//////          .where("topic", isEqualTo: "flutter")
-////        .snapshots()
-////        .listen((data) =>
-////        data.documents.forEach((doc) => print(doc["deviceId"]))); //Parking location
-//
-//
-////
-////    for (var VehicleId in gateRecords.documents) {
-////      print(VehicleId.data['paymentStatus']);
-////    }
-//
-//    //if phoneNumber in userRecords = "+85212345678" --> gateRecords: entryConfirmTime is before now() and exitConfirmTime is null
-//    // --> get vehicleId, iotStateChanges: Parking location, Duration = entryconfirm - now,
-//    // fees
-//
-//    //vehicleId
-//    FirebaseUser user;
-////    print(user.phoneNumber);
-//    var now = new DateTime.now();
-//    var vehicleId;
-//    var parkingLocation;
-//    var parkingDuration;
-//    num parkingDurationInMinutes;
-//    var parkingFee;
-//    QuerySnapshot gateRecords;
-//    QuerySnapshot iotStateChanges;
-//    var parkingInfo = Map();
-////    List<String> parkingInfo = [];
-//
-//    String _printDuration(Duration duration) {
-//      String twoDigits(int n) {
-//        if (n >= 10) return "$n";
-//        return "0$n";
-//      }
-//
-//      String twoDigitMinutes = twoDigits(duration.inMinutes.remainder(60));
-//      String twoDigitSeconds = twoDigits(duration.inSeconds.remainder(60));
-//      return "${twoDigits(duration.inHours)}:$twoDigitMinutes:$twoDigitSeconds";
-//    }
-//
-//    QuerySnapshot userRecords = await Firestore.instance
-//        .collection('userRecords')
-//        .where("phoneNumber", isEqualTo:  "+85212345678") //need change to user.phoneNumber
-//        .getDocuments();
-//
-//    List<DocumentSnapshot> matchUserRecords = userRecords.documents.toList();
-//
-//    for (var matchUserRecord in matchUserRecords) {
-////        print(matchUserRecord.data['vehicleId']);
-//
-//    //TODO:Uncomment
-////        gateRecords = await Firestore.instance
-////            .collection('gateRecords')
-////            .where("vehicleId", isEqualTo: matchUserRecord.data['vehicleId'])
-////            .where("entryConfirmTime", isLessThanOrEqualTo:  now)
-////            .where('exitConfirmTime', isEqualTo: null)
-////            .getDocuments();
-//
-//        gateRecords = await Firestore.instance
-//            .collection('gateRecords')
-//            .getDocuments();
-//
-//        for (var gateRecord in gateRecords.documents) {
-//          //TODO: delete
-//          if (!gateRecord.data.containsKey('entryConfirmTime')) {
-//            continue;
-//          }
-//          var entryConfirmTime = gateRecord.data['entryConfirmTime'].toDate();
-//          vehicleId = gateRecord.data['vehicleId'];
-//          parkingDuration = now.difference(entryConfirmTime);
-//          parkingDurationInMinutes = parkingDuration.inMinutes;
-//          parkingFee = (Math.min(120, parkingDurationInMinutes)/60) * 20 + (Math.max(0, parkingDurationInMinutes - 120)/60) * 50;
-//
-//          iotStateChanges = await Firestore.instance
-//              .collection('iotStateChanges')
-//              .getDocuments();
-//
-//          for (var iotStateChange in iotStateChanges.documents) {
-//            var previousState = iotStateChange.data['previousState'];
-//            if (previousState['vehicleId'] == vehicleId) {
-//              parkingLocation = iotStateChange['deviceId'];
-//            }
-//          }
-//        }
-//
-//        parkingInfo['vehicleId'] = vehicleId;
-//        parkingInfo['parkingLocation'] = parkingLocation;
-//        parkingInfo['parkingDuration'] = _printDuration(parkingDuration);
-//        parkingInfo['parkingFee'] = parkingFee.round().toString();
-//    }
-//  }
-
   String vehicleId = "";
   String parkingLocation = "";
   String parkingDuration = "";
   String parkingFee = "";
+  String entryConfirmTime = "";
+  String exitConfirmTime = "";
 
-    _getData() async {
-      getData('vehicleId').then((String val){
-        setState(() {
-          vehicleId = val;
-        });
+  _getData() async {
+    getData('vehicleId').then((String val){
+      setState(() {
+        vehicleId = val;
       });
-      getData('parkingLocation').then((String val){
-        setState(() {
-          parkingLocation = val;
-        });
+    });
+    getData('parkingLocation').then((String val){
+      setState(() {
+        parkingLocation = val;
       });
-      getData('parkingDuration').then((String val){
-        setState(() {
-          parkingDuration = val;
-        });
+    });
+    getData('parkingDuration').then((String val){
+      setState(() {
+        parkingDuration = val;
       });
-      getData('parkingFee').then((String val){
-        setState(() {
-          parkingFee = val;
-        });
+    });
+    getData('parkingFee').then((String val){
+      setState(() {
+        parkingFee = val;
       });
-    }
+    });
+    getData('entryConfirmTime').then((String val){
+      setState(() {
+        parkingFee = val;
+      });
+    });
+    getData('exitConfirmTime').then((String val){
+      setState(() {
+        parkingFee = val;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
 
     var now = new DateTime.now();
     String formattedDate = DateFormat('yyyy-MM-dd').format(now);
-
-//    void getValue() {
-//      for (var doc in Firestore.instance.collection("userRecords").document('')) {
-//        print(doc.data);
-//      }
-//    }
 
     Widget snapShot = SafeArea(
       child: Row(
@@ -198,181 +84,6 @@ class _TimeLinePageState extends State<TimeLinePage> {
     );
 
     Widget infoTable = Container(
-//      child: Padding(
-//        padding: EdgeInsets.symmetric(horizontal: 10.0),
-//        child: Table(children: [
-//          TableRow(children: [
-//            TableCell(
-//              child: Row(
-//                mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                children: <Widget>[
-//                  Text('Car Plate Number:'),
-//                  Text('PK1234'),
-//                  SizedBox(width:55),
-//                ],
-//              ),
-//            ),
-//          ]),
-//          TableRow(children: [
-//            TableCell(
-//              child: Row(
-//                mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                children: <Widget>[
-//                  Text('Parking Location:'),
-//                  Text('LG5 - 1'),
-//                  SizedBox(width:55),
-//                ],
-//              ),
-//            ),
-//          ]),
-//          TableRow(children: [
-//            TableCell(
-//              child: Row(
-//                mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                children: <Widget>[
-//                  Text('Parking Duration:'),
-//                  Text('30 mins'),
-//                  SizedBox(width:55),
-//                ],
-//              ),
-//            ),
-//          ]),
-//          TableRow(children: [
-//            TableCell(
-//              child: Row(
-//                mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                children: <Widget>[
-//                  Text('Parking Fees:'),
-//                  Container(
-//                    child: Row(
-//                      children: <Widget>[
-//                        Text('HKD 30 '),
-//                        Container(
-//                          width: 55,
-//                          height: 15,
-//                          child: RaisedButton(
-//                            onPressed: () {},
-//                            child: Text(
-//                                "Pay",
-//                                style: TextStyle(
-//                                  fontSize: 12,
-//                                  color: Colors.black,
-//                                )
-//                            ),
-//                          ),
-//                        )
-//                      ],
-//                    ),
-//                  ),
-//                ],
-//              ),
-//            ),
-//          ]),
-//          TableRow(children: [
-//            TableCell(
-//              child: Row(
-//                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                children: <Widget>[
-//                  Container(
-//                    width: 150,
-//                    height: 15,
-//                    child: RaisedButton(
-//                      onPressed: () {},
-//                      child: Text(
-//                          "Parking Records",
-//                          style: TextStyle(
-//                            fontSize: 12,
-//                            color: Colors.black,
-//                          )
-//                      ),
-//                    ),
-//                  ),
-//                  Container(
-//                    width: 150,
-//                    height: 15,
-//                    child: RaisedButton(
-//                      onPressed: () {},
-//                      child: Text(
-//                          "Request Snapshot",
-//                          style: TextStyle(
-//                            fontSize: 12,
-//                            color: Colors.black,
-//                          )
-//                      ),
-//                    ),
-//                  )
-//                ],
-//              ),
-//            ),
-//          ]),
-//        ]),
-//      ),
-//
-//        child: Row(
-//
-//          children: <Widget>[
-//            SizedBox(width: 80),
-//            Expanded(
-//
-//              flex: 1,
-//              child: Column(
-//
-//                mainAxisSize: MainAxisSize.min,
-//                crossAxisAlignment: CrossAxisAlignment.start,
-////                mainAxisAlignment: MainAxisAlignment.start,
-//                children: <Widget>[
-//                  Text('Car Plate Number:'),
-//                  Text('Parking Location:'),
-//                  Text('Parking Duration:'),
-//                  Text('Parking Fees:'),
-//                ],
-//              ),
-//            ),
-//            Expanded(
-//              flex: 1,
-//              child: Column(
-//                mainAxisSize: MainAxisSize.min,
-//                crossAxisAlignment: CrossAxisAlignment.start,
-////                mainAxisAlignment: MainAxisAlignment.start,
-//                children: <Widget> [
-//                  Text('HA2213'),
-//                  Text('LG5'),
-//                  Text('30 mins'),
-////                  Text('test'),
-////                  Expanded(
-//
-//                    Row(
-//                      children: <Widget>[
-//                        Text('HKD 30 '),
-//                        SizedBox(
-//                          width: 57,
-//                          height: 25,
-//                          child: ButtonTheme(
-////                    minWidth: 10.0,
-////                    height: 10.0,
-//                            child: RaisedButton(
-//                              onPressed: () {},
-//                              child: Text("Pay"),
-//                            ),
-//                          ),
-//                        ),
-////                        ButtonTheme(
-////                          minWidth: 10.0,
-////                          height: 10.0,
-////                          child: RaisedButton(
-////                            onPressed: () {},
-////                            child: Text("Pay"),
-////                          ),
-////                        ),
-//                      ],
-//                    ),
-////                  ),
-//                ],
-//              ),
-//            ),
-//          ],
-//        )
-
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
 //          mainAxisAlignment: MainAxisAlignment.start,
@@ -432,7 +143,7 @@ class _TimeLinePageState extends State<TimeLinePage> {
 //                    minWidth: 10.0,
 //                    height: 10.0,
                       child: RaisedButton(
-                        onPressed: () => getData('vehicleId'),
+                        onPressed: (){},
                         child: Text(
                             "Pay",
                             style: TextStyle(
@@ -534,31 +245,17 @@ class _TimeLinePageState extends State<TimeLinePage> {
         )
     );
 
-
-
-
-//    return StreamProvider<QuerySnapshot>.value(
-//      value: IotStates().iotStates,
     return Scaffold (
       body: Column (
           children: [
             infoSection,
             dateSection,
             timeLine,
-//              Timeline(
-//                  children: items,
-//                  position: TimelinePosition.Center
-//              ),
           ],
         ),
       );
   }
   Future<String> getData(data) async {
-    //if phoneNumber in userRecords = "+85212345678" --> gateRecords: entryConfirmTime is before now() and exitConfirmTime is null
-    // --> get vehicleId, iotStateChanges: Parking location, Duration = entryconfirm - now,
-    // fees
-
-    //vehicleId
     FirebaseUser user;
 //    print(user.phoneNumber);
     var now = new DateTime.now();
@@ -567,6 +264,13 @@ class _TimeLinePageState extends State<TimeLinePage> {
     var parkingDuration;
     num parkingDurationInMinutes;
     var parkingFee;
+
+    //
+    var entryConfirmTime;
+    var exitConfirmTime;
+    var parkingTime;
+
+
     QuerySnapshot gateRecords;
     QuerySnapshot iotStateChanges;
 //      var parkingInfo = Map();
@@ -590,7 +294,6 @@ class _TimeLinePageState extends State<TimeLinePage> {
     List<DocumentSnapshot> matchUserRecords = userRecords.documents.toList();
 
     for (var matchUserRecord in matchUserRecords) {
-//        print(matchUserRecord.data['vehicleId']);
 
       //TODO:Uncomment
 //        gateRecords = await Firestore.instance
@@ -609,7 +312,7 @@ class _TimeLinePageState extends State<TimeLinePage> {
         if (!gateRecord.data.containsKey('entryConfirmTime')) {
           continue;
         }
-        var entryConfirmTime = gateRecord.data['entryConfirmTime'].toDate();
+        entryConfirmTime = gateRecord.data['entryConfirmTime'].toDate();
         vehicleId = gateRecord.data['vehicleId'];
         parkingDuration = now.difference(entryConfirmTime);
         parkingDurationInMinutes = parkingDuration.inMinutes;
@@ -644,6 +347,14 @@ class _TimeLinePageState extends State<TimeLinePage> {
           return await parkingFee.round().toString();
         }
         break;
+//        case 'entryConfirmTime':{
+//          return await entryConfirmTime;
+//        }
+//        break;
+//        case 'exitConfirmTime':{
+//          return await exitConfirmTime;
+//        }
+//        break;
       }
 //        parkingInfo['vehicleId'] = vehicleId;
 //        parkingInfo['parkingLocation'] = parkingLocation;

e98ebb578bff2bacfd1e15fed062b6a25c22a698
Esmond <esmond9981127@gmail.com>
2020-04-13 14:18:53 +0800

    wip: card validation

--- mobile/lib/pages/payment/add_credit_card_page.dart
+++ mobile/lib/pages/payment/add_credit_card_page.dart
@@ -1,7 +1,9 @@
 import 'package:awesome_card/credit_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:awesome_card/awesome_card.dart' as card;
 import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:stripe_sdk/stripe_sdk_ui.dart';
 
 class AddCreditCardPage extends StatefulWidget {
   final String gateRecordId;
@@ -16,11 +18,10 @@ class AddCreditCardPage extends StatefulWidget {
 }
 
 class _AddCreditCardPageState extends State<AddCreditCardPage> {
-  String cardNumber = "";
-  String expiryDate = "";
-  String cardHolderName = "";
-  String cvv = "";
+  StripeCard _card = StripeCard(number: "", cvc: "");
   FocusNode _cvvFocusNode = FocusNode();
+
+  bool _saveCardDetails = true;
 
   MaskedTextController _cardNumberTextController =
       MaskedTextController(mask: '0000 0000 0000 0000');
@@ -42,6 +43,14 @@ class _AddCreditCardPageState extends State<AddCreditCardPage> {
   void dispose() {
     _cvvFocusNode.dispose();
     super.dispose();
+  }
+
+  bool _isCardValid() {
+    return this._card.validateCard();
+  }
+
+  void _payWithCard() {
+
   }
 
   @override
@@ -70,14 +79,14 @@ class _AddCreditCardPageState extends State<AddCreditCardPage> {
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 20.0),
                   child: CreditCard(
-                    cardNumber: cardNumber,
-                    cardExpiry: expiryDate,
-                    cardHolderName: cardHolderName,
-                    cvv: cvv,
-                    bankName: "",
+                    cardNumber: this._card.number,
+                    cardExpiry: null,
+                    cardHolderName: this._card.name,
+                    cvv: this._card.cvc,
                     showBackSide: this._cvvFocusNode.hasFocus,
-                    frontBackground: card.CardBackgrounds.black,
+                    frontBackground: card.CardBackgrounds.white,
                     backBackground: card.CardBackgrounds.white,
+                    frontTextColor: Colors.black,
                     showShadow: true,
                   ),
                 ),
@@ -85,25 +94,28 @@ class _AddCreditCardPageState extends State<AddCreditCardPage> {
                   padding: const EdgeInsets.symmetric(horizontal: 24.0),
                   child: Column(
                     children: <Widget>[
-                      TextFormField(
-                        decoration: InputDecoration(labelText: "Card Number", counterText: ""),
-                        controller: this._cardNumberTextController,
-                        maxLength: 19,
-                        keyboardType: TextInputType.number,
-                        onChanged: (value) {
-                          setState(() {
-                            cardNumber = value;
-                          });
-                        },
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 8.0),
+                        child: TextFormField(
+                          decoration: InputDecoration(labelText: "Card Number", counterText: ""),
+                          controller: this._cardNumberTextController,
+                          maxLength: 19,
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            setState(() {
+                              this._card.number = value;
+                            });
+                          },
+                        ),
                       ),
                       Padding(
-                        padding: const EdgeInsets.only(bottom: 14.0),
+                        padding: const EdgeInsets.only(bottom: 8.0),
                         child: TextFormField(
                           decoration:
                               InputDecoration(labelText: "Card Holder Name"),
                           onChanged: (value) {
                             setState(() {
-                              cardHolderName = value;
+                              this._card.name = value;
                             });
                           },
                         ),
@@ -122,7 +134,7 @@ class _AddCreditCardPageState extends State<AddCreditCardPage> {
                                 maxLength: 5,
                                 onChanged: (value) {
                                   setState(() {
-                                    expiryDate = value;
+//                                    expiryDate = value;
                                   });
                                 },
                               ),
@@ -139,7 +151,7 @@ class _AddCreditCardPageState extends State<AddCreditCardPage> {
                                 maxLength: 3,
                                 onChanged: (value) {
                                   setState(() {
-                                    cvv = value;
+                                    this._card.cvc = value;
                                   });
                                 },
                               ),
@@ -148,6 +160,18 @@ class _AddCreditCardPageState extends State<AddCreditCardPage> {
                         ],
                       ),
                     ],
+                  ),
+                ),
+                ListTile(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 24.0),
+                  title: Text("Save card details"),
+                  trailing: CupertinoSwitch(
+                    value: this._saveCardDetails,
+                    onChanged: (value) {
+                      setState(() {
+                        this._saveCardDetails = value;
+                      });
+                    },
                   ),
                 ),
                 Container(
@@ -162,7 +186,7 @@ class _AddCreditCardPageState extends State<AddCreditCardPage> {
                         Icon(Icons.navigate_next)
                       ],
                     ),
-                    onPressed: () => {},
+                    onPressed: this._isCardValid() ? this._payWithCard : null,
                     splashColor: Colors.redAccent,
                   ),
                 )

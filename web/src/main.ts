f3ddc1806e067a4ad3bbd726a28590223ff68611
Esmond <esmond9981127@gmail.com>
2020-04-20 17:46:43 +0800

    wip: added openlayers library

--- web/src/main.ts
+++ web/src/main.ts
@@ -5,11 +5,14 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
 import { firestorePlugin } from 'vuefire'
+import VueLayers from 'vuelayers'
+import 'vuelayers/lib/style.css'
 
 Vue.config.productionTip = false
 
 Vue.use(ElementUI, { locale })
 Vue.use(firestorePlugin)
+Vue.use(VueLayers)
 
 new Vue({
   router,
--- web/src/views/Live.vue
+++ web/src/views/Live.vue
@@ -1,5 +1,14 @@
 <template>
   <div class='live'>
+    <el-row >
+      <vl-map :load-tiles-while-animating="true" :load-tiles-while-interacting="true"
+             data-projection="EPSG:4326" style="height: 400px; padding-bottom: 16px">
+      <vl-view :zoom.sync="zoom" :center.sync="center" :rotation.sync="rotation"></vl-view>
+      <vl-layer-tile id="osm">
+        <vl-source-osm></vl-source-osm>
+      </vl-layer-tile>
+    </vl-map>
+    </el-row>
     <el-row>
       <el-button @click="manualEntry">Manual Entry</el-button>
     </el-row>
@@ -18,10 +27,18 @@
 <script>
 import moment from 'moment'
 import { db } from '@/helpers/firebaseHelper'
+
 export default {
   name: 'Live',
   data () {
     return {
+      zoom: 16,
+      center: [114.263270, 22.338616],
+      rotation: 0,
+      polygon: {
+        latlngs: [[47.2263299, -1.6222], [47.21024000000001, -1.6270065], [47.1969447, -1.6136169], [47.18527929999999, -1.6143036], [47.1794457, -1.6098404], [47.1775788, -1.5985107], [47.1676598, -1.5753365], [47.1593731, -1.5521622], [47.1593731, -1.5319061], [47.1722111, -1.5143967], [47.1960115, -1.4841843], [47.2095404, -1.4848709], [47.2291277, -1.4683914], [47.2533687, -1.5116501], [47.2577961, -1.5531921], [47.26828069, -1.5621185], [47.2657179, -1.589241], [47.2589612, -1.6204834], [47.237287, -1.6266632], [47.2263299, -1.6222]],
+        color: 'green'
+      },
       gateRecords: []
     }
   },

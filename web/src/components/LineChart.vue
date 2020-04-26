75a218b149fc236cdfc19ede24a761f960f9326f
Elliot Ng <chihimng.io@gmail.com>
2020-04-26 16:31:45 +0800

    fix: not displaying

--- web/src/components/LineChart.vue
+++ web/src/components/LineChart.vue
@@ -1,39 +1,55 @@
 <script>
 import { Line } from 'vue-chartjs'
+
 export default {
-  name: 'Line Chart',
   extends: Line,
-  mounted () {
-    this.renderChart(
-      {
-        labels: [
-          'January',
-          'February',
-          'March',
-          'April',
-          'May',
-          'June',
-          'July'
-        ],
+  data () {
+    return {
+      chartdata: {
+        // Data to be represented on x-axis
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
         datasets: [
           {
-            label: 'Data 1',
-            data: [2, 10, 5, 9, 0, 6, 20],
-            backgroundColor: 'transparent',
-            borderColor: 'rgba(1, 116, 188, 0.50)',
-            pointBackgroundColor: 'rgba(171, 71, 188, 1)'
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: '#249EBF',
+            // Data to be represented on y-axis
+            data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
           }
         ]
       },
-      {
+      // Chart.js options that controls the appearance of the chart
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [{
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: true
+        },
         responsive: true,
-        maintainAspectRatio: false,
-        title: {
-          display: true,
-          text: 'My Data'
-        }
+        maintainAspectRatio: false
       }
-    )
+    }
+  },
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
   }
 }
 </script>
+
+<style>
+</style>
--- web/src/views/Stats.vue
+++ web/src/views/Stats.vue
@@ -1,81 +1,17 @@
 <template>
-  <div class="stats">
+  <div>
     <h1>This is the stats view</h1>
-    <div class="Chart">
-      <h1 style="text-align:center;">Linechart</h1>
-        <LineChart/>
-      <!-- <line-chart :chartdata="chartData" :options="chartOptions"/> -->
-    </div>
-    <!-- <line-chart :chartdata="chartdata" :options="options"/> -->
-    <!-- v-if="loaded"  -->
+    <line-chart></line-chart>
   </div>
 </template>
 
 <script>
-// import VueCharts from 'vue-chartjs'
-import { LineChart } from '../components/LineChart.vue'
-// import { Line, mixins, generateChart } from 'vue-chartjs'
-import { db, Timestamp } from '@/helpers/firebaseHelper'
-// const { reactiveProp } = mixins
-// const CustomLine = generateChart('line-chart', 'line')
+import LineChart from '@/components/LineChart'
+
 export default {
   name: 'Stats',
   components: {
-    LineChart
-  },
-  data () {
-    return {
-      chartData: {
-        Books: 24,
-        Magazine: 30,
-        Newspapers: 10
-      }
-    }
-  },
-  // extends: Line,
-  // mixins: [reactiveProp],
-  // props: {
-  //   chartdata: {
-  //     type: Object,
-  //     default: null
-  //   },
-  //   options: {
-  //     type: Object,
-  //     default: null
-  //   }
-  // },
-  // data: () => ({
-  //   chartdata: {
-  //     labels: ['January', 'February'],
-  //     datasets: [
-  //       {
-  //         label: 'Data One',
-  //         backgroundColor: '#f87979',
-  //         data: [40, 20]
-  //       }
-  //     ]
-  //   },
-  //   options: {
-  //     responsive: true,
-  //     maintainAspectRatio: false
-  //   }
-  // }),
-  // mounted () {
-  //   this.renderChart(this.chartdata, this.options)
-  // },
-  // data: () => ({
-  //   loaded: false,
-  //   chartdata: null
-  // }),
-  // async mounted () {
-  //   this.loaded = false
-  //   try {
-  //     // const { userlist } = await fetch('/api/userlist')
-  //     // this.chartdata = userlist
-  //     // this.loaded = true
-  //   } catch (e) {
-  //     console.error(e)
-  //   }
-  // }
+    'line-chart': LineChart
+  }
 }
 </script>

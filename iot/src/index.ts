5ca43dc595d0806fa722e8d1232bad8d2de1eff6
Esmond <esmond9981127@gmail.com>
2020-05-14 22:37:59 +0800

    fix: final param adjustment for iot

--- iot/src/index.ts
+++ iot/src/index.ts
@@ -20,7 +20,7 @@ const mode: string = process.env.mode
 const deviceId: string = process.env.deviceId
 const gateMode: string = process.env.gateMode
 const gateThresholdInCm: number = 250
-const lotThresholdInCm: number = 100
+const lotThresholdInCm: number = 150
 const stableThresholdInCm: number = 5 // TODO: find out error / noise range of reading
 const historySize: number = 3
 
@@ -31,7 +31,7 @@ function average(arr: number[]) {
 }
 
 async function takePicture() : Promise<Buffer> {
-  const camera = await exec('raspistill -q 10 -ISO 800 -ex sports -n -o ./snapshot.jpg -t 300')
+  const camera = await exec('raspistill -q 10 -ISO 800 -ex sports -n -o ./snapshot.jpg -t 500')
   if (!camera.stderr) {
     return await readFile('./snapshot.jpg')
   } else {
@@ -45,7 +45,7 @@ async function main() {
     const distanceHistory: number[] = []
     const firebaseHelper = new FirebaseHelper(deviceId)
     await firebaseHelper.init()
-    const distanceHelper = new DistanceHelper(300)
+    const distanceHelper = new DistanceHelper()
 
     switch (mode) {
       case Mode.gate: {

a49a873dd905575dc02feebbdf672a0145a25198
cheungkayigrace <gracecheung1203@gmail.com>
2020-05-15 16:10:13 +0800

    update Stats.vue

--- web/src/views/Stats.vue
+++ web/src/views/Stats.vue
@@ -1,68 +1,38 @@
 <template>
   <div>
-    <h1>Parking hour</h1>
-    <line-chart v-if="loaded" :chartdata="chartdata" :options="options"/>
-    <!-- <h1>Entry Time</h1>
-    <line-chart></line-chart>
-    <h1>Exit Time</h1>
-    <line-chart></line-chart>
-    <h1>Parking duration</h1>
-    <line-chart></line-chart>
-    <h1>Peak hour:</h1> -->
+    <!-- <table>
+      <th>Peaking Hour</th>
+      <tr><td>hi</td></tr>
+    </table> -->
+    <!-- <h1 align=center>Entry Time</h1> -->
+    <entry-line-chart :chartdata="chartdata" :options="options"/>
+    <br><br>
+    <exit-line-chart :chartdata="chartdata" :options="options"/>
+    <br><br>
+    <duration-line-chart :chartdata="chartdata" :options="options"/>
+    <br><br>
+    <parking-hour-line-chart :chartdata="chartdata" :options="options"/>
   </div>
 </template>
 
 <script>
-import LineChart from '@/components/LineChart'
-import moment from 'moment'
-import { db, Timestamp } from '@/helpers/firebaseHelper'
+import entryLineChart from '@/components/entryLineChart'
+import exitLineChart from '@/components/exitLineChart'
+import durationLineChart from '@/components/durationLineChart'
+import parkingHourLineChart from '@/components/parkingHourLineChart'
 
 export default {
   name: 'Stats',
   components: {
-    'line-chart': LineChart
+    'entry-line-chart': entryLineChart,
+    'exit-line-chart': exitLineChart,
+    'duration-line-chart': durationLineChart,
+    'parking-hour-line-chart': parkingHourLineChart
   },
   data: () => ({
     loaded: false,
-    chartdata: null
-  }),
-  async mounted () {
-    this.loaded = false
-    try {
-      const snapshot = await db
-        .collection('iotStateChanges')
-        // FIXME: for getting records of current day, index building...
-        // .where('time', '>=', Timestamp.fromDate(moment().startOf('day').toDate()))
-        // .where('time', '<=', Timestamp.fromDate(moment().endOf('day').toDate()))
-        // .where('previousState.state', '==', 'vacant')
-        // .where('newState.state', '==', 'occupied')
-        .limit(100)
-        .get()
-      snapshot.docs.forEach((doc) => console.log(doc.data()))
-      // const iotStateChanges = await
-      // const previousState = await db.collection('iotStateChange').doc().collection('previousState').get().then(
-      //   (data) => {
-      //   }
-      // )
-      //   function (querySnapshot) {
-      //   querySnapshot.forEach (function (doc) {
-      //     // doc.data() is never undefined for query doc snapshots
-      //     console.log(doc.id, '-->', doc.data())
-      //   })
-      // })
-      // const newState = iotStateChanges.doc().collection('newState')
-      // const { entryTimeList } = await iotStateChanges.where('previousState/state', '==', 'vacant')
-      // .where(iotStateChanges.doc().collection('newState').state, '==', 'occupied')
-      // console.log(previousState)
-      // console.log(newState)
-      // this.chartdata = entryTimeList
-      // this.loaded = true
-      // const { userlist } = await fetch('/api/userlist')
-      // this.chartdata = userlist
-      // this.loaded = true
-    } catch (e) {
-      console.error(e)
-    }
-  }
+    chartdata: null,
+    options: null
+  })
 }
 </script>
